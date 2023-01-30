7131942c3b5447ebd60a230abeaf690cc15077f6
mackbdev <mackenziebakshdev@gmail.com>
2023-01-30 04:30:35 +0000

    classic fizzbuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -15,6 +15,9 @@ public class FizzBuzz {
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
     public String FizzBuzzGen(int n){
+        if (n % 15 == 0){ return "FizzBuzz";}
+        if (n % 3 == 0){ return "Fizz";}
+        if (n % 5 == 0){ return "Buzz";}
         return "";
     }
 }
